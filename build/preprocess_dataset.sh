c5fe2fdedfa134e7a198eb56985e51fd65b63b99
Ilsoo Seong <issung83@gmail.com>
2019-09-17 17:25:31 -0700

    add print

--- build/preprocess_dataset.sh
+++ build/preprocess_dataset.sh
@@ -3,4 +3,5 @@ python -c "from Reco3D.lib import dataset
 if __name__ == '__main__':
     dataset.setup_dir()
     dataset.prepare_dataset()
-    dataset.preprocess_dataset()"
+    dataset.preprocess_dataset()
+    print ('done')"
