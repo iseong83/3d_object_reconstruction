21a5291a4cda2c3fee73ae1023de4a478f67a526
Ilsoo Seong <issung83@gmail.com>
2019-09-22 18:32:38 -0700

    fix epoch counting

--- Reco3D/lib/utils.py
+++ Reco3D/lib/utils.py
@@ -47,11 +47,10 @@ def get_latest_epoch_index(model_dir):
     if is_epoch_dir(model_dir):
         model_dir = os.path.dirname(model_dir)
     # Modified to get the lastest epoch even though it doesn't contain all epochs
+
     folder_list = sorted(next(os.walk(model_dir))[1], reverse=True)
-    for mdir in folder_list:
-        if 'epoch' in mdir:
-            return int(mdir.split('_')[1])
-    return -1
+    epoch_list = sorted([int(i.split('_')[1]) for i in folder_list[2:]])
+    return epoch_list[0] if len(epoch_list)>0 else -1
 
 
 def get_latest_epoch(model_dir):
