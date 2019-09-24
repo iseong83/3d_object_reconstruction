992b08ab671fca58276c1ca11c8bb6e12a9fc9ea
Ilsoo Seong <issung83@gmail.com>
2019-09-23 23:25:51 -0700

    rename demo and inference_deeplab

--- demo.py
+++ demo.py
@@ -10,52 +10,66 @@ import matplotlib.pyplot as plt
 import Reco3D.lib.dataset as dataset
 import Reco3D.lib.network as network
 import Reco3D.lib.vis as vis
-from Reco3D.lib import metrics
+from Reco3D.lib.segmentation import *
+from PIL import Image
 import argparse
 import time
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--path", help="The model path", 
+    parser.add_argument("--reco_model", help="The model path", 
             type=str, default='./models_local/model_2019-09-15_22:25:37/epoch_153')
+    parser.add_argument("--seg_model", help="The model name for image semenation", 
+            type=str, default='mobile_coco')
     parser.add_argument("--data", help="Example data path", 
-            type=str, default='./examples/chair_a')
-    parser.add_argument("--rnd", help="If want to make with random dataset", action='store_true', default=False)
-
- 
+            type=str, default='./examples/chair_z')
     args = parser.parse_args()
     return args
 
-def load_images(img_path, n_views=5):
+def load_images(img_path, model, n_views=5):
     # load example images
+    size = 245, 245
     print ('Loading Images at {}'.format(img_path))
-    filenames = list(set(os.path.join(img_path,n) for n in os.listdir(img_path) if n.endswith(".png")))
-    # let's shuffle the images
-    img_data = dataset.load_imgs(filenames)
+    original_img = list()
+    segmented_img = list()
+    for root, _, files in os.walk(img_path):
+        for i, file in enumerate(files):
+            if i >= n_views: break
+            img = load_image(os.path.join(root,file))
+            resized_im, seg_map = model.run(img)
+            mask = np.repeat(seg_map[:,:,np.newaxis],3,axis=2)
+            object = np.where(mask>0, np.array(resized_im), 0)
+            # resize them
+            #object = Image.fromarray(object)
+            #resized_im.thumbnail(size, Image.ANTIALIAS)
+            #object.thumbnail(size, Image.ANTIALIAS)
+
+            original_img.append(resized_im)
+            segmented_img.append(object)
     print ("Loaded example")
-    return img_data
+    return np.stack(original_img), np.stack(segmented_img)
 
 def main():
     args = get_args()
-    model_dir = args.path.strip('/')
-    image_dir = args.data
-    random_data = args.rnd
+    reco_model = args.reco_model.strip('/')
+    seg_model  = args.seg_model
+    image_dir  = args.data
 
-    print ('Loading the model {}'.format(model_dir))
-    net=network.Network_restored(model_dir)
+    print ('Loading the models {} {}'.format(reco_model, seg_model))
+    net = network.Network_restored(reco_model)
+    seg = DeepLabModel(seg_model)
     print ('Loaded the model')
+    
+    org_img, seg_img = load_images(image_dir, seg)
+    print (org_img.shape, seg_img.shape)
 
-    if not random_data:
-        X = load_images(image_dir)
-    else:
-        X, Y = dataset.load_random_sample()
-    print (X.shape)
     # show example image
-    vis.multichannel(X[0])
+    vis.multichannel(org_img[0])
+    #vis.img_sequence(org_img)
 
     # make inference
     t1 = time.time()
-    out = net.predict(X[:,:,:,0:3])
+    out = net.predict(seg_img[:,:,:,0:3])
     t2 = time.time()
     
     print ("Inference time {} sec".format(t2-t1))
